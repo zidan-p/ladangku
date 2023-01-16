d03675fea940ae54b5371e7b2ee0581374390bdc
zidan-p <zidanputrarahman153@gmail.com>
2023-01-16 18:28:19 +0700

    style monitor 2 fail

--- src/components/main/Monitor/Monitor.js
+++ src/components/main/Monitor/Monitor.js
@@ -27,32 +27,58 @@ export default function Monitor(){
     }
 
     return (
-        <div className=" h-full flex flex-col overflow-y-auto">
-            <div className="px-12 h-2/5 ">
-                <div className=" overflow-auto h-full">
-                    {/* <IndonesianMap className="w-full" /> */}
-                    {/* <img src={IndonesianMap} /> */}
-                    <img className="w-full" src="https://upload.wikimedia.org/wikipedia/commons/5/5a/Indonesia_provinces_blank_map.svg" />
-                </div>
-            </div>
+        // <div className=" h-full flex flex-col overflow-y-auto">
+        //     <div className="px-12 h-2/5 ">
+        //         <div className=" overflow-auto h-full">
+        //             {/* <IndonesianMap className="w-full" /> */}
+        //             {/* <img src={IndonesianMap} /> */}
+        //             <img className="w-full" src="https://upload.wikimedia.org/wikipedia/commons/5/5a/Indonesia_provinces_blank_map.svg" />
+        //         </div>
+        //     </div>
 
-            {/* //section untuk harga */}
-            <div className="px-12 my-7 flex h-full overflow-y-auto border-l-4 border-l-green-600 border-y py-2">
-                <div className="basis-2/3 pr-5 overflow-y-auto">
+        //     {/* //section untuk harga */}
+        //     <div className="px-12 my-7 flex h-full overflow-y-auto border-l-4 border-l-green-600 border-y py-2">
+        //         <div className="basis-2/3 pr-5 overflow-y-auto">
+        //             <h4 className="mb-2 font-semibold text-lg">Daftar Provinsi</h4>
+        //             <div className="grid grid-cols-2 gap-2">
+        //                 {hargaDagang.map((dagangan,i) => {
+        //                     return <ProvinceList handleOnClick={handleChangeActive} name={dagangan.name} idProvinsi={i} key={i}/>
+        //                 })}
+        //             </div>
+
+        //         </div>
+        //         <div className="basis-1/3 pl-5 border-l border-gray-400">
+        //             { activeRegion === null
+        //             ? <h1 className="text-center font-semibold">Belum memilih daerah</h1>
+        //             : <ProvincePrice activeProvince={activeRegion} />
+        //             }
+        //         </div>
+        //     </div>
+        // </div>
+
+        <div className=" h-full flex flex-col overflow-y-auto bg-gray-100">
+            <div className="px-12 my-7 flex gap-3 h-full overflow-y-auto  py-2">
+                <div className="basis-2/3 bg-white p-3">
+                <img className="w-full" src="https://upload.wikimedia.org/wikipedia/commons/5/5a/Indonesia_provinces_blank_map.svg" />
+                    <div className="">
+                        { activeRegion === null
+                        ? <h1 className="text-center font-semibold">Belum memilih daerah</h1>
+                        : <ProvincePrice activeProvince={activeRegion} />
+                        }
+                    </div>
+                </div>
+                <div className="basis-1/3 overflow-y-auto bg-white p-2">
                     <h4 className="mb-2 font-semibold text-lg">Daftar Provinsi</h4>
-                    <div className="grid grid-cols-2 gap-2">
-                        {hargaDagang.map((dagangan,i) => {
-                            return <ProvinceList handleOnClick={handleChangeActive} name={dagangan.name} idProvinsi={i} key={i}/>
-                        })}
+                    <div className="h-full overflow-y-auto">
+                        <div className="grid grid-cols-1 gap-2">
+                            {hargaDagang.map((dagangan,i) => {
+                                return <ProvinceList handleOnClick={handleChangeActive} name={dagangan.name} idProvinsi={i} key={i}/>
+                            })}
+                        </div>
                     </div>
 
                 </div>
-                <div className="basis-1/3 pl-5 border-l border-gray-400">
-                    { activeRegion === null
-                    ? <h1 className="text-center font-semibold">Belum memilih daerah</h1>
-                    : <ProvincePrice activeProvince={activeRegion} />
-                    }
-                </div>
+            
             </div>
         </div>
     )
